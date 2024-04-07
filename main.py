d8dbd8bde3c55f2ebe4cba1d08043e0b0a1bff0d
GHST981 <166331506+GHST981@users.noreply.github.com>
2024-04-07 14:02:28 -0600

    agegamos arroba y .com

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import random
 
 adjectives = ["Cool", "Mystic", "Electric", "Wild", "Groovy"]
 
-nouns = ["gmail"]
+nouns = ["gmail",]
 
 def generate_band_name():
     adjective = random.choice(adjectives)
     noun = random.choice(nouns)
-    return f"{adjective} {noun}"
+    return f"{adjective}@{noun}.com"
 
 band_name = generate_band_name()
-band_name
+print(band_name)
